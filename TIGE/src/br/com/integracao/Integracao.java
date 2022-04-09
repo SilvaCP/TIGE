303b93c7727b10fd5fc903c37a1f30dbeadcad6e
SilvaCP <79268743+SilvaCP@users.noreply.github.com>
2022-04-08 23:54:55 -0300

    Acrescentado a quarta linha.

--- TIGE/src/br/com/integracao/Integracao.java
+++ TIGE/src/br/com/integracao/Integracao.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Vai dar certo!");
 	System.out.println("Vai dar certo!");
 	System.out.println("Vai dar certo!");
+	System.out.println("Vai dar certo!");
 }
 }
