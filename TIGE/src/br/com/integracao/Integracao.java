a7f563f8349f5ba4ddb1588fa11be7131131a2c3
Carlos <Carlos@DESKTOP-F3GM3EK>
2022-04-08 23:56:49 -0300

    Reduzi em apena uma linha.

--- TIGE/src/br/com/integracao/Integracao.java
+++ TIGE/src/br/com/integracao/Integracao.java
@@ -3,8 +3,6 @@ package br.com.integracao;
 public class Integracao {
 public static void main(String[] args) {
 	System.out.println("Vai dar certo!");
-	System.out.println("Vai dar certo!");
-	System.out.println("Vai dar certo!");
-	System.out.println("Vai dar certo!");
+	
 }
 }
