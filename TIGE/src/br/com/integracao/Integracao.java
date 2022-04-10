4970608e9561fd3f861f599effe7eadc68b9efab
SilvaCP <79268743+SilvaCP@users.noreply.github.com>
2022-04-10 20:16:07 -0300

    Fiz uma correção ortográfica, na linha 6.

--- TIGE/src/br/com/integracao/Integracao.java
+++ TIGE/src/br/com/integracao/Integracao.java
@@ -3,7 +3,7 @@ package br.com.integracao;
 public class Integracao {
 public static void main(String[] args) {
 	System.out.println("Vai dar certo!");
-	System.out.println("Matematica");
+	System.out.println("Matemática");
 	
 }
 }
